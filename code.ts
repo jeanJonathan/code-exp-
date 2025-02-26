ca3bbac9e74b189a29a17dc75aa095a1be61c043
jean-jonathan KOFFI <99023611+jeanJonathan@users.noreply.github.com>
2025-02-26 10:00:44 +0100

    Update code.ts

--- code.ts
+++ code.ts
@@ -4,6 +4,6 @@ SELECT
     "Sales Document Type" AS SalesDocumentType,
     "SD document category" AS SDDocCategory,
     "Shipping Conditions" AS ShippingConditions
-FROM "Nom_de_ta_base".VBAK
+FROM "ZMC500_CV_VBAK_P_ADV01"
 WHERE "Date on Which Record Was Created" BETWEEN '20200101' AND '20251231'
-AND "Shipping Conditions" = 'EX'
+AND "Shipping Conditions" = 'EX';
